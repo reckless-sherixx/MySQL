9d92b6f102581c59604510af300221cef1bc0adf
Vidyansh <ezboi2312@gmail.com>
2025-07-01 23:02:19 +0530

    MySQL

---
+++ GroupBy_OrderBy_Statements.sql
@@ -0,0 +1,18 @@
+-- Group By
+
+SELECT * 
+FROM employee_demographics;
+
+SELECT gender , AVG(age) , MAX(age) , MIN(age) , COUNT(age)
+FROM employee_demographics
+GROUP BY gender ;
+
+-- ORDER BY
+SELECT * 
+FROM employee_demographics
+-- ORDER BY first_name DESC;
+ORDER BY gender , age DESC;
+
+
+
+
