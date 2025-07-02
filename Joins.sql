79995204940cc7d2065e2700d646d09b2575bdae
Vidyansh <ezboi2312@gmail.com>
2025-07-03 00:26:28 +0530

    MySQL

---
+++ Joins.sql
@@ -0,0 +1,53 @@
+-- Joins
+
+-- Inner Joins
+
+SELECT dem.employee_id , age , occupation 
+FROM employee_demographics AS DEM 
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- Outer Joins
+
+SELECT dem.employee_id , age , occupation 
+FROM employee_demographics AS DEM 
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- Left Join
+
+SELECT * 
+FROM employee_demographics AS dem 
+LEFT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- Right Join
+
+SELECT * 
+FROM employee_demographics AS dem 
+RIGHT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- Self Join 
+
+SELECT emp1.employee_id AS emp_santa,
+emp1.first_name AS first_name_santa,
+emp1.last_name AS last_name_santa,    
+emp2.employee_id AS emp_name,
+emp2.first_name AS first_name_emp,
+emp2.last_name AS last_name_emp
+FROM employee_salary emp1
+JOIN employee_salary emp2
+	ON emp1.employee_id + 1 = emp2.employee_id;
+    
+-- Multiple Joins
+
+SELECT * 
+FROM employee_demographics AS dem 
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+INNER JOIN parks_departments pd
+	ON sal.dept_id = pd.department_id ;
+    
+SELECT * 
+FROM parks_departments
